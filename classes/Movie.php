1e5ac5eeaea1f0bca60584690f87105660ab6a79
gabrisca <gabrielescarparo1985@gmail.com>
2021-06-18 10:20:00 +0200

    commenti

--- classes/Movie.php
+++ classes/Movie.php
@@ -11,7 +11,13 @@ class Movie {
   public $cast;
   private $storyline; // private obbliga ad utilizzare una funzione per accedere alla proprietà (linee 26 e 35)
   static $counter = 0; // 0 come valore di default
-
+  
+  /**
+   * constructor
+   *
+   * @param [type] $_title
+   * @param [type] $_year
+   */
   public function __construct($_title, $_year)
   {
     $this->title = $_title;
@@ -24,6 +30,12 @@ class Movie {
   /////////////////////////
   
   // funzione che mi permette di avere accesso ad una proprietà private
+  /**
+   * setStoryline
+   *
+   * @param [type] $_storyline
+   * @return void
+   */
   public function setStoryline($_storyline){
     $this->storyline = $_storyline;
   }
@@ -33,11 +45,21 @@ class Movie {
    /////////////////////////
 
   // funzione che mi permette di stampare il valore di una proprietà private
+  /**
+   * getStoryline
+   *
+   * @return void
+   */
   public function getStoryline(){
     return $this->storyline;
   }
 
   // funzione che restituisce il titolo e l'(anno) del film
+  /**
+   * getInfo
+   *
+   * @return void
+   */
   public function getInfo(){ 
     return $this->title .' ('. $this->year . ')';
   }
